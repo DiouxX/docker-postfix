b46d3366f40f97c041ecf4da342a38bcd82edd50
DiouxX <github@diouxx.be>
2017-07-07 10:16:37 +0200

    /etc/cyrus.conf -> OK

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,10 @@ ENV DEBIAN_FRONTEND noninteractive
 RUN apt update \
 && apt -y install nano cyrus-admin cyrus-clients cyrus-common cyrus-imapd libauthen-sasl-cyrus-perl libcyrus-imap-perl sasl2-bin
 
+#Config file
+COPY config/cyrus.conf /etc/
+
+#Config Script
 COPY postfix-start.sh /opt/
 RUN chmod +x /opt/postfix-start.sh
 
--- postfix-start.sh
+++ postfix-start.sh
@@ -12,6 +12,13 @@ sed -i -e "s/autocreate_quota: 0/autocreate_quota: -1/g" /etc/imapd.conf
 sed -i -e '/sasl_mech_list: PLAIN/s/^#//g' /etc/imapd.conf
 sed -i -e "s/sasl_pwcheck_method: auxprop/sasl_pwcheck_method: saslauthd/g" /etc/imapd.conf
 
+#File /etc/cyrus.conf
+#delete tabulation
+sed -i -e '/imaps          cmd="imapd -s -U 30" listen="imaps" prefork=0 maxchild=100/s/#//g' /etc/cyrus.conf
+sed -i -e '/\tpop3            cmd="pop3d -U 30" listen="pop3" prefork=0 maxchild=50/s/^/#/g' /etc/cyrus.conf
+sed -i -e '/\tsieve           cmd="timsieved" listen="localhost:sieve" prefork=0 maxchild=100/s/^/#/g' /etc/cyrus.conf
+sed -i -e '/#\tsieve           cmd="timsieved" listen="localhost:sieve" prefork=0 maxchild=100/a sieve           cmd="timsieved" listen="sieve" prefork=1 maxchild=100' /etc/cyrus.conf
+
 #File /etc/default/saslautd
 sed -i -e "s/\START=no/\START=yes/g" /etc/default/saslauthd
 
