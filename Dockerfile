b70c256b3a73fa36a9756a83d50f7cc1a72a41c1
DiouxX <github@diouxx.be>
2017-07-11 11:07:08 +0200

    File /etc/sasauthd.conf -> OK

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,12 @@ MAINTAINER DiouxX "github@diouxx.be"
 
 #Ne pas poser de question à l'installation
 ENV DEBIAN_FRONTEND noninteractive
+ENV LDAP_SERVER ldap.acme.com
+ENV LDAP_PORT 389
+ENV DOMAIN acme.com
+ENV LDAP_BIND uid=cyrus,dc=acme,dc=com
+ENV LDAP_BIND_PW password
+ENV LDAP_SEARCH_BASE ou=Users,dc=acme,dc=com
 
 RUN apt update \
 && apt -y install nano cyrus-admin cyrus-clients cyrus-common cyrus-imapd libauthen-sasl-cyrus-perl libcyrus-imap-perl sasl2-bin
--- postfix-start.sh
+++ postfix-start.sh
@@ -19,6 +19,9 @@ sed -i -e '/\tpop3            cmd="pop3d -U 30" listen="pop3" prefork=0 maxchild
 sed -i -e '/\tsieve           cmd="timsieved" listen="localhost:sieve" prefork=0 maxchild=100/s/^/#/g' /etc/cyrus.conf
 sed -i -e '/#\tsieve           cmd="timsieved" listen="localhost:sieve" prefork=0 maxchild=100/a sieve           cmd="timsieved" listen="sieve" prefork=1 maxchild=100' /etc/cyrus.conf
 
+#File /etc/saslauthd.conf
+echo -e "# SERVEUR LDAP\nLDAP_SERVERS: ldap://$LDAP_SERVER:$LDAP_PORT\n\n#DOMAINE\n#LDAP_DEFAULT_DOMAIN: $DOMAIN\n\nLDAP_TIMEOUT: 10\nLDAP_TIME_LIMIT: 10\nLDAP_CACHE_TTL: 30\nLDAP_CACHE_MEM: 32768\n\n#VERSION LDAP\nLDAP_VERSION: 3\n\n# SASL Pour l'accès au serveur\nLDAP_USE_SASL: no\n#LDAP_START_TLS: yes\n#LDAP_TLS_CACERT_DIR: /root/openldap01.pem\n\n# Méthode d'authentification (bind / custom / fastbind)\nLDAP_AUTH_METHOD: bind\n\n# Utilisateur utilisé pour la connexion - Si vide = Anonyme\nLDAP_BIND_DN: $LDAP_BIND\n# Et le mot de passe\nLDAP_BIND_PW: $LDAP_BIND_PW\n\n# Base de départ de la recherche\nLDAP_SEARCH_BASE: $LDAP_SEARCH_BASE\n# Et profondeur (sub / one / base )\nLDAP_SCOPE: sub\n#Filtre de recherche : uid dans notre cas\nLDAP_FILTER: uid=%u\n# Et nom du champ contenant le mot de passe\nLDAP_PASSWORD_ATTR: userPassword" > /etc/saslauthd.conf
+
 #File /etc/default/saslautd
 sed -i -e "s/\START=no/\START=yes/g" /etc/default/saslauthd
 
